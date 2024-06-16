9d1d4fb5ea8f510fd3662da73251bcb2c505e035
Shnok <skyler.maki@gmail.com>
2024-06-17 02:38:03 +0800

    grass update, camera positions, windmill

---
+++ l2-unity/Assets/Scripts/Rendering/Effect/Windmill.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using System.Collections;
+
+public class Windmill : MonoBehaviour {
+    public float rotateSpeed = 1f;
+    void Update() {
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z + Time.deltaTime * rotateSpeed);
+    }
+}
