a08579b486f34f3edeabec908c02d38157266a78
shnok <skyler.maki@gmail.com>
2024-11-23 19:20:52 +0800

    revert to 6000.0.16f1

--- l2-unity/Assets/Plugins/FMOD/src/RuntimeUtils.cs
+++ l2-unity/Assets/Plugins/FMOD/src/RuntimeUtils.cs
@@ -417,8 +417,8 @@ namespace FMODUnity
             if (rigidbody)
             {
                 FMOD.VECTOR vel;
-                vel.x = rigidbody.velocity.x;
-                vel.y = rigidbody.velocity.y;
+                vel.x = rigidbody.linearVelocity.x;
+                vel.y = rigidbody.linearVelocity.y;
                 vel.z = 0;
                 attributes.velocity = vel;
             }
@@ -434,8 +434,8 @@ namespace FMODUnity
             if (rigidbody)
             {
                 FMOD.VECTOR vel;
-                vel.x = rigidbody.velocity.x;
-                vel.y = rigidbody.velocity.y;
+                vel.x = rigidbody.linearVelocity.x;
+                vel.y = rigidbody.linearVelocity.y;
                 vel.z = 0;
                 attributes.velocity = vel;
             }
