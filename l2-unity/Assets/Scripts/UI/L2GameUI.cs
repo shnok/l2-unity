b2425cc9e4fe1efc1e0f24daaf1228902f4cf6d7
shnok <skyler.maki@gmail.com>
2024-11-28 11:33:35 +0800

    now cursor is correctly locked

--- l2-unity/Assets/Scripts/UI/L2GameUI.cs
+++ l2-unity/Assets/Scripts/UI/L2GameUI.cs
@@ -131,6 +131,7 @@ public class L2GameUI : L2UI
         {
             _mouseEnabled = true;
             NativeFunctions.SetCursorPos(_lastMousePosition.X, _lastMousePosition.Y);
+            // UnityEngine.Cursor.lockState = CursorLockMode.None;
         }
     }
 
@@ -139,7 +140,12 @@ public class L2GameUI : L2UI
         if (_mouseEnabled)
         {
             NativeFunctions.GetCursorPos(out _lastMousePosition);
+            // UnityEngine.Cursor.lockState = CursorLockMode.Locked;
             _mouseEnabled = false;
+        }
+        else
+        {
+            NativeFunctions.SetCursorPos(_lastMousePosition.X, _lastMousePosition.Y);
         }
     }
 
