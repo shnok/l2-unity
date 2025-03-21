4b969321800deb4b5a2b8ecb0afe2f99be798f46
shnok <skyler.maki@gmail.com>
2025-03-21 20:56:32 +0800

    fixed last commits induced errors

--- l2-unity/Assets/Scripts/Animation/Controller/NewBaseAnimationController.cs
+++ l2-unity/Assets/Scripts/Animation/Controller/NewBaseAnimationController.cs
@@ -27,11 +27,6 @@ public abstract class NewBaseAnimationController : MonoBehaviour
     protected AnimancerState _animancerState;
     public float PAtkSpd { get => _atkSpd; }
 
-    private void Awake()
-    {
-        Initialize();
-    }
-
     public virtual void Initialize()
     {
         if (_entityReferenceHolder == null)
